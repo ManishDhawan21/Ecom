8a576864d81c38e6807a723d53030491a69a28a3
manish <mk33933@gmail.com>
2025-09-12 12:14:11 +0530

    my changes

--- app.js
+++ app.js
@@ -20,7 +20,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(compression());
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
-app.use('/images',express.static(__dirname+'/Public/images'))
+app.use('/images',express.static(__dirname+'Public/images'))
+
+app.use(express.static(path.join(__dirname,"dist")))
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname,"dist","index.html"))
+})
 
 //signup & signin
 app.use('/api/v2/account',accountRouter)
