1695fbe922e163f9d4eac9a4897879c702e14f2c
manish <mk33933@gmail.com>
2025-09-15 11:15:24 +0530

    P change in folder

--- app.js
+++ app.js
@@ -20,14 +20,14 @@ app.use(express.urlencoded({ extended: true }));
 app.use(compression());
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename);
-app.use('/images',express.static(__dirname+'Public/images'))
+app.use('/images',express.static(__dirname+'public/images'))
 
 app.use(express.static(path.join(__dirname,"dist")))
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname,"dist","index.html"))
 })
 
-//signup & signin
+//signup & signinn
 app.use('/api/v2/account',accountRouter)
 //admin
 app.use('/api/v2/admin',adminRouter)
